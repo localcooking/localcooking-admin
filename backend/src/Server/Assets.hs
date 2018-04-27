6c23f72fb33dd5401b7f20aa86c759537b54bc2a
Athan Clark <athan.clark@localcooking.com>
2018-04-26 18:35:49 -0600

    using admin icon

--- backend/src/Server/Assets.hs
+++ backend/src/Server/Assets.hs
@@ -18,7 +18,7 @@ frontendMin :: ByteString
 frontendMin = $(embedFile "../frontend/index.min.js")
 
 favicons :: [(FilePath, ByteString)]
-favicons = $(embedDir "../logo/localcooking-chef-favicon")
+favicons = $(embedDir "../logo/localcooking-admin-favicon")
 
 images :: [(FilePath, ByteString)]
 images = $(embedDir "images")
